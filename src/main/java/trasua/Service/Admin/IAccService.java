a53b70b1bab7fca8a0877ea7db285dfe9314f6b5
Phuong Nam <pnam1910@gmail.com>
2022-03-21 18:05:10 +0700

    update admin

--- src/main/java/trasua/Service/Admin/IAccService.java
+++ src/main/java/trasua/Service/Admin/IAccService.java
@@ -1,17 +1,15 @@
-package trasua.Service.User;
+package trasua.Service.Admin;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import trasua.Entity.loaisanpham;
+
 import trasua.Entity.taikhoan;
 
 @Service
-public interface IAccountService {
+public interface IAccService {
 	@Autowired
-	public int AddAccount (taikhoan user);
-	public taikhoan CheckAccount(taikhoan user);
-
+	public List<taikhoan> GetDataAcc();
 }
--- src/main/java/trasua/Service/User/IHomeService.java
+++ src/main/java/trasua/Service/User/IHomeService.java
@@ -15,4 +15,6 @@ public interface IHomeService {
 	
 	public List<loaisanpham> GetDataloaiSP();
 	
+	
+	
 }
